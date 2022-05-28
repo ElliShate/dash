127cedfda9f7f2bda1f1392f41e300e46fc696eb
Elion Shate <elion.shategit@gmail.com>
2022-05-28 12:40:43 +0200

    init

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartData} from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Ecommerce = () => {
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="w-full bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl  lg:w-80 pt-8 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex flex-col justify-between items-start pl-5">
+            <p className="font-bold text-gray-400">Earnings</p>
+            <p className="text-2xl">$63,448.78</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Ecommerce
